bfe1e6f6b0e0eab0f8fef2d6e846a882b3e04f12
ytxc0 <jakob.leistner@gmail.com>
2024-07-24 19:50:10 +0200

    Provided functions for the map

---
+++ map.js
@@ -0,0 +1,107 @@
+let clickedCountryLayer;
+
+// Create map
+let map = L.map('map', { worldCopyJump: true }).setView([26.75, 14.25], 2);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);
+
+// Add clickable countries to map
+let geojson = L.geoJson(countriesGeoJson, {
+    style: {
+        fillColor: "#8884",
+        weight: 1,
+        opacity: 1,
+        color: "#0000",
+        dashArray: '3',
+        fillOpacity: 0.7
+    },
+    onEachFeature: handleFeatureClick
+}).addTo(map);
+
+// Create marker
+let marker = L.marker([0, 0]);
+
+// Map click event
+map.on('click', (e) =>
+{
+    let lat = e.latlng.lat;
+    let lng = e.latlng.lng;
+
+    console.log(lat, lng);
+    map.addLayer(marker);
+    marker.setLatLng(e.latlng);
+});
+
+
+function highlightSelectedFeature(layer)
+{
+    layer.setStyle({
+        fillColor: "#1F77BA",
+        weight: 1,
+        color: '#004177',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    layer.bringToFront();
+}
+
+function highlightCorrectCountry()
+{
+    Object.keys(map._layers).forEach(layerNumber =>
+    {
+        let layer = map._layers[layerNumber];
+        if (layer instanceof L.Path)
+        {
+            let iso = layer.feature.properties.ISO_A3;
+            if (iso == currentFlag || iso == adminCountries[currentFlag])
+            {
+                layer.setStyle({
+                    fillColor: "#2a2",
+                    weight: 1,
+                    color: "#070",
+                    dashArray: '',
+                    fillOpacity: 0.7
+                });
+                layer.bringToFront();
+
+                console.log(layer);
+                map.fitBounds(layer._latlngs);
+            }
+        }
+    });
+}
+
+function highlightWrongCountry()
+{
+    clickedCountryLayer.setStyle({
+        fillColor: "#C52124",
+        weight: 1,
+        color: '#7F2518',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    clickedCountryLayer.bringToFront();
+}
+
+function resetMap()
+{
+    geojson.resetStyle();
+    map.removeLayer(marker);
+    map.setView([26.75, 14.25], 2);
+}
+
+function handleFeatureClick(feature, layer)
+{
+    layer.on({
+        click: (e) =>
+        {
+            geojson.resetStyle();
+
+            clickedCountryLayer = e.target;
+            highlightSelectedFeature(clickedCountryLayer);
+        }
+    });
+}
